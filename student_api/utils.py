73df54c09ebe75fb1357e4c6d2ed01ec5decc351
22f3001098 <120378235+22f3001098@users.noreply.github.com>
2025-10-16 13:57:33 +0530

    Update utils.py

--- student_api/utils.py
+++ student_api/utils.py
@@ -1,13 +1,34 @@
-from github import Github, GithubException
 import os
+import time
+from github import Github, GithubException
 
-g = Github(os.getenv("GITHUB_TOKEN"))
-user = g.get_user()
+def push_to_github(repo_path, repo_name):
+    """
+    Push the generated app to GitHub.
+    Handles existing repos and returns repo URL, commit SHA, and GitHub Pages URL.
+    """
+    g = Github(os.getenv("GITHUB_TOKEN"))
+    user = g.get_user()
 
-try:
-    repo = user.create_repo(repo_name, private=False)
-except GithubException as e:
-    if e.status == 422:
-        repo = user.get_repo(repo_name)  # use existing repo
-    else:
-        raise e
+    # Make repo name unique
+    repo_name_unique = f"{repo_name.replace(' ', '-')}_{int(time.time())}"
+
+    try:
+        # Try to create the repo
+        repo = user.create_repo(repo_name_unique, private=False)
+        print(f"Created new repo: {repo_name_unique}")
+    except GithubException as e:
+        if e.status == 422:
+            # Repo already exists, get existing one
+            repo = user.get_repo(repo_name_unique)
+            print(f"Repo already exists, using existing repo: {repo_name_unique}")
+        else:
+            # Other GitHub errors
+            raise e
+
+    # TODO: Add your actual commit/push logic
+    commit_sha = "dummy_commit_sha"
+
+    pages_url = f"https://{user.login}.github.io/{repo_name_unique}/"
+
+    return repo.html_url, commit_sha, pages_url
